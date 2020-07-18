3ce744101e15f809aea7bb573c1dee472ddebd9f
John Rossi <DACOOK1983@gamil.com>
2020-07-17 22:08:07 -0400

    re-installed all dependencies, issues with previous build

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
+const projects = require("./routes/projectsRoutes");
 
 const server = express();
 server.use(express.json());
@@ -10,5 +11,7 @@ server.get('/', (req,res) =>{
     <h1>My first web api sprint challenge</h1>
     `)
 });
+
+server.use("/api/projects", projects);
 
 module.exports = server;
